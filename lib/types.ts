e6b86053f401e25e0dbf7d05b676b785c632069c
germi1986 <gerarguello86@gmail.com>
2026-05-06 00:46:10 -0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -2,15 +2,12 @@ export type Product = {
   id: string;
   name: string;
   brand?: string;
-  category?: string;
+  category?: string; // esto sigue siendo el género
+  mainCategory?: string; // 👈 NUEVO (Perfumería, Cremas, Vapeadores)
   description?: string;
   price: number;
   priceArs?: number;
   stock?: number;
   imageUrl?: string;
   sku?: string;
-};
-
-export type CartItem = Product & {
-  quantity: number;
 };
